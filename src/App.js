e049af90ca151fde1960f83a0fc808578482c25e
Aryan Sengar <92522070+Aryan-sengar@users.noreply.github.com>
2023-10-15 15:19:25 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div>
+      <Calculator></Calculator>
+    </div>
+  );
+}
+
+export default App;
